eb44105301cff0ff4bec2480486e9d3715cdc531
giauu <133954934+giauu@users.noreply.github.com>
2023-05-18 15:40:39 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
+  text1: "Chào bé! Phan Thị Diễm Nhi!",
+  text2: "Anh có điều muốn nói với bé. Bé trả lời anh thật lòng nha.",
+  text3: "Bé có yêu anh không <3",
+  text4: "Bé hỏng có được thoát đâu nhơ.. Thoát là làm Wife anh đó nhơ ! ",
+  text5: "Hong bé ơi???",
+  text6: "Có. Tình yêu to bự luôn <3",
+  text7: "Tại sao bé lại yêu anh :vvvv",
+  text8: "Thân gửi ANH YÊU <3",
   text9: "Vì Cậu Quá Đẹp Trai. HÍ HÍ",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text10: "Anh biết mà ^^ Yêu 3000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Tối nay Anh qua chở Bé đi chơi nhơ. còn dì nữa mà hỏng nhắn cho anh.Chụt Chụt ",
+  text12: "Dạ Anh Yêu <3",
 };
 
 $(document).ready(function () {
